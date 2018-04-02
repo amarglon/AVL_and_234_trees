3464cd1bff90805fe36259c6384479ba9c0c7612
amarglon <31377395+amarglon@users.noreply.github.com>
2018-04-02 08:28:46 -0700

    added 234 tree search function and get() method

--- src/Proj04_234_student.java
+++ src/Proj04_234_student.java
@@ -1,6 +1,54 @@
+import java.io.IOException;
 
 public class Proj04_234_student implements Proj04_Dictionary {
-
+	//TODO insert, traversals, transfers and rotations
+	//TAKE THIS OUT 
+	//http://www.cs.armstrong.edu/liang/intro11e/html/Tree24WithLineNumber.html?
+	public Proj04_234_student () {
+		root = null;
+	}
+	
+	@Override
+	public String search(int key) {
+		Proj04_234Node traversal = root;
+		while (traversal != null) {
+			if (key == traversal.key1) {
+				return traversal.val1;
+			} else if (key == traversal.key2) {
+				return traversal.val2;
+			} else if (key == traversal.key3) {
+				return traversal.val3;
+			} else {
+				traversal = getChild(key, traversal);
+			}
+		}
+		return null;
+	}
+	
+	public  Proj04_234Node getChild(int key, Proj04_234Node traversal) {
+		if (traversal.childA == null) {
+			return null;
+		} 
+		traversal = get(key, traversal);
+		return traversal;
+	}
+	
+	private Proj04_234Node get(int key, Proj04_234Node traversal) {
+		Proj04_234Node[] childArray = {traversal.childA, traversal.childB, traversal.childC, traversal.childD};
+		for (Proj04_234Node node : childArray) {
+			if (key <= node.key1) {
+				return node;
+			}
+			else if (key <= node.key2) {
+				return node;
+			}
+			else if (key <= node.key3) {
+				return node;
+			}
+		}
+		return null;
+	}
+	
 	@Override
 	public void insert(int key, String value) {
 		// TODO Auto-generated method stub
@@ -8,15 +56,23 @@ public class Proj04_234_student implements Proj04_Dictionary {
 	}
 
 	@Override
-	public String search(int key) {
-		// TODO Auto-generated method stub
-		return null;
+	public void delete(int key) { 
+		try {
+			deleteHelper(key);
+		} catch (CannotDeleteException e) {
+			e.printStackTrace();
+		}
 	}
+	private void deleteHelper(int key) throws CannotDeleteException {
+		throw new CannotDeleteException("Delete has not been defined for 234 trees");
+	}
+	
+	class CannotDeleteException extends Exception {
+		private static final long serialVersionUID = 1L;
 
-	@Override
-	public void delete(int key) {
-		// TODO Auto-generated method stub
-
+		CannotDeleteException(String message){
+			System.out.println(message);
+		}
 	}
 
 	@Override
@@ -39,8 +95,12 @@ public class Proj04_234_student implements Proj04_Dictionary {
 
 	@Override
 	public void genDebugDot(String filename) {
-		// TODO Auto-generated method stub
-
+		try {
+			Proj04_GenDotFile.gen(root, filename);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("genDebugDot(): File handling error Proj04_BST_student class");
+		}
 	}
-
+	private Proj04_234Node root;
 }
